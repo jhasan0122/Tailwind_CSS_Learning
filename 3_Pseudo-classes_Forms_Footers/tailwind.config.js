28670019e921a24a2bece68df9888ea9d2c7fd22
Jehan Hasan <jhasan0122@gmail.com>
2024-08-22 14:28:33 +0600

    chap 3

---
+++ 3_Pseudo-classes_Forms_Footers/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        'widescreen' : {'raw': '(min-aspect-ration: 3/2)'},
+        'tallscreen' : {'raw': '(min-aspect-ration: 1/2)'},
+      }
+    },
+  },
+  plugins: [],
+}
+
